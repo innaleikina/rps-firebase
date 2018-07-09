a12e0c1bb5984fa251dabd1c57899be8a131a072
Inna <designbyinna@gmail.com>
2018-07-09 08:38:39 -0600

    newer version

--- resources/js/index.js
+++ resources/js/index.js
@@ -15,6 +15,35 @@ $(document).ready(function () {
     var player1Name;
     var player2Name;
 
+    var connectionsRef = database.ref("/connections");
+    var connectedRef = database.ref(".info/connected");
+
+    connectedRef.on("value", function (snap) {
+
+        // If they are connected..
+        if (snap.val()) {
+            console.log(database.connections);
+            // console.log(Object.keys(connectionsRef));
+            // Add user to the connections list.
+            var con = connectionsRef.push(true);
+            // if (database.connections > 2) {
+            //     console.log("game is full");
+            // }
+
+            // Remove user from the connection list when they disconnect.
+            con.onDisconnect().remove();
+        }
+    });
+
+    connectionsRef.on("value", function (snap) {
+
+        // Display the viewer count in the html.
+        // The number of online users is the number of children in the connections list.
+        $("#connected-viewers").text(snap.numChildren());
+    });
+
+
+
     var localUserObj = {
         player1: {
             name: player1Name,
@@ -32,80 +61,83 @@ $(document).ready(function () {
         }
     }
 
-    var player1Present = false;
-    var player2Present = false;
 
-    function checkNumberOfPlayers() {
+    console.log(localUserObj.length);
+        var player1Present = false;
+        var player2Present = false;
+
+        function checkNumberOfPlayers() {
+            usersRef.on("value", function (snapshot) {
+                player1Present = snapshot.val().player1.player1Present,
+                    player2Present = snapshot.val().player2.player2Present
+
+                if (player1Present && player2Present) {
+                    $("#input").hide();
+                    $("#addPlayer").hide();
+                    console.log("player 1 " + player1Present);
+                    console.log("player 2 " + player2Present);
+
+                }
+            });
+        }
+
+        checkNumberOfPlayers();
+
+        $("#addPlayer").on("click", function () {
+            //if player 1 is not present upadte player 1's name in the databse and replace it on the front end
+            event.preventDefault();
+
+            if (!player1Present && !player2Present) {
+                player1Name = $("#input").val().trim();
+                $("#player1-name").text(player1Name);
+                database.ref("/users/player1").set({
+                    name: $("#input").val().trim(),
+                    player1Present: true,
+                });
+                usersRef.on("value", function (snapshot) {
+                    player1Present = snapshot.val().player1.player1Present
+                });
+                database.ref("users/player1").onDisconnect().remove();
+
+                //if player one is present, update player 2 in the database and in the front-end
+            } else if (player1Present && !player2Present) {
+                player2Name = $("#input").val().trim()
+                $("#player2-name").text(player2Name);
+                database.ref("/users/player2").set({
+                    name: $("#input").val().trim(),
+                    player2Present: true,
+                });
+                usersRef.on("value", function (snapshot) {
+                    player2Present = snapshot.val().player2.player2Present
+                });
+                checkNumberOfPlayers();
+                database.ref("users/player2").onDisconnect().remove();
+
+
+            } else if (player1Present && player2Present) {
+                alert("there are already two players here");
+                checkNumberOfPlayers();
+            }
+        })
+
         usersRef.on("value", function (snapshot) {
-            player1Present = snapshot.val().player1.player1Present,
-                player2Present = snapshot.val().player2.player2Present
+            $("#player1-name").text(snapshot.val().player1.name);
+            // console.log(snapshot.val().player1.name)
+            $("#player2-name").text(snapshot.val().player2.name);
 
-            if (player1Present && player2Present) {
-                $("#input").hide();
-                $("#addPlayer").hide();
-                console.log("player 1 " + player1Present);
-                console.log("player 2 " + player2Present);
 
+        })
+
+
+
+
+        var connectedRef = firebase.database().ref(".info/connected");
+
+        connectedRef.on("value", function (snap) {
+            if (snap.val() === true) {
+                database.ref("/users/" + playerNumber).remove();
+            } else {
+                 alert("not connected");
             }
         });
-    }
-
-    checkNumberOfPlayers();
-
-    $("#addPlayer").on("click", function () {
-        //if player 1 is not present upadte player 1's name in the databse and replace it on the front end
-        event.preventDefault();
-
-        if (!player1Present && !player2Present) {
-            player1Name = $("#input").val().trim();
-            $("#player1-name").text(player1Name);
-            database.ref("/users/player1").set({
-                name: $("#input").val().trim(),
-                player1Present: true,
-            });
-            usersRef.on("value", function (snapshot) {
-                player1Present = snapshot.val().player1.player1Present
-            });
-
-
-            //if player one is present, update player 2 in the database and in the front-end
-        } else if (player1Present && !player2Present) {
-            player2Name = $("#input").val().trim()
-            $("#player2-name").text(player2Name);
-            database.ref("/users/player2").set({
-                name: $("#input").val().trim(),
-                player2Present: true,
-            });
-            usersRef.on("value", function (snapshot) {
-                player2Present = snapshot.val().player2.player2Present
-            });
-            checkNumberOfPlayers();
-
-        } else if (player1Present && player2Present) {
-            alert("there are already two players here");
-            checkNumberOfPlayers();
-        }
-    })
-
-    usersRef.on("value", function (snapshot) {
-        $("#player1-name").text(snapshot.val().player1.name);
-        // console.log(snapshot.val().player1.name)
-        $("#player2-name").text(snapshot.val().player2.name);
-
-
-    })
-
-
-
-
-    var connectedRef = firebase.database().ref(".info/connected");
-
-    connectedRef.on("value", function (snap) {
-        if (snap.val() === true) {
-            // alert("connected");
-        } else {
-            // alert("not connected");
-        }
-    });
-
 });
